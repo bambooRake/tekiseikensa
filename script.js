9b9efa923d3187de0b09311e75a5dd57a7e14dc1
bamb <bamboolev1@icloud.com>
2023-10-06 19:44:57 +0900

    適性検査

---
+++ script.js
@@ -0,0 +1,54 @@
+let circle = document.getElementById('circle');
+let carSound = document.getElementById('carSound');
+let brakeSound = document.getElementById('brakeSound');
+
+// 初期状態として青に設定して、次に切り替えるまでの待ち時間を設定
+setBlue();
+
+function setRed() {
+    circle.classList.remove('blue');
+    circle.classList.add('red');
+
+    // 加速音を停止
+    carSound.pause();
+    carSound.currentTime = 0;
+
+    // ブレーキ音を再生
+    brakeSound.play();
+
+    let timeoutDuration = getRandomTime(2000, 7000); // 2-5秒
+    setTimeout(setBlue, timeoutDuration);
+}
+
+function setBlue() {
+    circle.classList.remove('red');
+    circle.classList.add('blue');
+
+    // ブレーキ音を停止
+    brakeSound.pause();
+    brakeSound.currentTime = 0;
+
+    // 加速音を再生
+    carSound.play();
+
+    let timeoutDuration = getRandomTime(5000, 12000); // 5-9秒
+    setTimeout(setRed, timeoutDuration);
+}
+
+function getRandomTime(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+adjustCircleSize();
+
+window.addEventListener('resize', adjustCircleSize); // ウィンドウサイズが変わったときも調整
+
+function adjustCircleSize() {
+    let vw = window.innerWidth;  // viewport width
+    let vh = window.innerHeight; // viewport height
+
+    let size = (vw < vh ? vw : vh) * 0.5; // 短い辺の30%
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+}
